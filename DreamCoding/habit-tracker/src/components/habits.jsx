67f1921a5d92671f28d953cbbdff09d41bd84a5b
Choi kyungyeul <dnjsvltm327@klnet.co.kr>
2022-04-20 18:19:59 +0900

    habit 컴포넌트 전체카운트

--- DreamCoding/habit-tracker/src/components/habits.jsx
+++ DreamCoding/habit-tracker/src/components/habits.jsx
@@ -1,46 +1,29 @@
-import React, { Component } from 'react'
-import Habit from './habit'
+import React, { Component } from "react";
+import Habit from "./habit";
 
 export default class Habits extends Component {
-    state = {
-        habits: [
-            {id:1,name: 'Reading',count:0},
-            {id:2,name: 'Running',count:0},
-            {id:3,name: 'Coding',count:0},
-        ]
-    }
-
-    handleIncrease = (habit) => {
-        const habits = [...this.state.habits];
-        const index = habits.indexOf(habit);
-        habits[index].count++;
-        this.setState({habits})
-    }
-    handleDecrease = (habit) => {
-        const habits = [...this.state.habits];
-        const index = habits.indexOf(habit);
-        const count = habits[index].count -1;
-        habits[index].count = count < 0 ? 0 :count;
-        this.setState({habits})
-    }
-    handleDelete = (habit) => {
-        const habits =this.state.habits.filter(item => item.id !== habit.id);
-        this.setState({habits});
-
-    }
+  handleIncrease = (habit) => {
+    this.props.onIncrement(habit);
+  };
+  handleDecrease = (habit) => {
+    this.props.onDecrement(habit);
+  };
+  handleDelete = (habit) => {
+    this.props.onDelete(habit);
+  };
   render() {
     return (
       <ul>
-          {this.state.habits.map((habit) => (
-              <Habit
-                habit={habit}
-                key={habit.id}
-                onIncrement={this.handleIncrease}
-                onDecrement={this.handleDecrease}
-                onDelete={this.handleDelete}
-               />
-          ))}
+        {this.props.habits.map((habit) => (
+          <Habit
+            habit={habit}
+            key={habit.id}
+            onIncrement={this.handleIncrease}
+            onDecrement={this.handleDecrease}
+            onDelete={this.handleDelete}
+          />
+        ))}
       </ul>
-    )
+    );
   }
 }
