232dbaf454cc66837dcb6d6f57635f53c9f0ffee
ChoiKY <dnjsvltm327@klnet.co.kr>
2022-07-01 16:53:36 +0900

    redux-thunk

--- reduxStudy/2.redux/actions/user.js
+++ reduxStudy/2.redux/actions/user.js
@@ -1,10 +1,37 @@
 const logIn = (payload) => {
-  console.log(payload);
+  return (dispatch, getState) => {
+    dispatch(loginRequest(payload));
+    try {
+      setTimeout(() => {
+        dispatch(loginSuccess({ userId: 1, name: "성공이름" }));
+      }, 2000);
+    } catch (err) {
+      dispatch(loginFailure(err));
+    }
+  };
+};
+
+const loginRequest = (payload) => {
   return {
-    type: "LOG_IN",
+    type: "LOG_IN_REQUEST",
     payload,
   };
 };
+
+const loginSuccess = (payload) => {
+  return {
+    type: "LOG_IN_SUCCESS",
+    payload,
+  };
+};
+
+const loginFailure = (err) => {
+  return {
+    type: "LOG_IN_FAILURE",
+    err,
+  };
+};
+
 const logOut = () => {
   return {
     type: "LOG_OUT",
--- reduxStudy/2.redux/index2.js
+++ reduxStudy/2.redux/index2.js
@@ -2,12 +2,8 @@ const { createStore, applyMiddleware } = require("redux");
 const reducer = require("./reducers/reducer");
 const { addPost } = require("./actions/post");
 const { logIn, logOut } = require("./actions/user");
-//객체의 뎁스가 action을 나누는 기준이 됨
-//때문에 이 구조를 잘짜는것이 중요하다
-//최대한 관련있는것은 묶어주고
-//메모리에 저장됨(나중에 메모리 관리를 해야되지만 크게 문제는 안된다)
-//왜냐하면 얕은복사를 하여 참조를 유지하기 때문에
-//그리고 데이터는 화면에 보일졍도만 가져오자
+
+//*****객체
 const initialState = {
   user: {
     isLoggingIn: true,
@@ -16,34 +12,42 @@ const initialState = {
   posts: [],
 };
 
-//미들웨어
+//*****미들웨어
 //중첩을  해둔 이유: 사이사이에 실행되는 시점을 위해서
-const firstMiddleWare = (store) => (dispatch) => (action) => {
+const firstMiddleWare = (store) => (next) => (action) => {
   console.log("액션로깅", action); //기본기능 실행전
-  dispatch(action); //기본기능
+  next(action); //기본기능
   console.log("액션끝");
 };
-const enhancer = applyMiddleware(firstMiddleWare);
 
+//redux thunk는 이게 다임(직접구현한 이것이랑 큰 차이가 없다)
+const thunkMiddleWare = (store) => (next) => (action) => {
+  if (typeof action === "function") {
+    //비동기인경우 action을 함수로
+    return action(store.dispatch, store.getState());
+  }
+  return next(action);
+};
+
+//미들웨어 여러개 붙여놓으면 순서대로 실행
+const enhancer = applyMiddleware(firstMiddleWare, thunkMiddleWare);
+
+//*****스토어
 const store = createStore(reducer, initialState, enhancer);
 
-store.subscribe(() => {
-  console.log("change");
-});
-
-console.log(store.getState());
+console.log("1st", store.getState());
 
 //*****dispatch
 store.dispatch(logIn({ id: 1, name: "cky", admin: true }));
 console.log("2th", store.getState());
 
-store.dispatch(logOut());
-console.log("3th", store.getState());
+// store.dispatch(logOut());
+// console.log("3th", store.getState());
 
-store.dispatch(addPost({ userId: 1, id: 1, content: "hello redux" }));
-console.log("4th", store.getState());
+// store.dispatch(addPost({ userId: 1, id: 1, content: "hello redux" }));
+// console.log("4th", store.getState());
 
-store.dispatch(addPost({ userId: 1, id: 2, content: "hello redux2222" }));
-console.log("4th", store.getState());
+// store.dispatch(addPost({ userId: 1, id: 2, content: "hello redux2222" }));
+// console.log("4th", store.getState());
 
 //cls
